8b084bfddddd7b275ddb9a32641dcb8eb71c56d8
LeoBoard <leokiller837@gmail.com>
2019-06-10 20:22:21 +0800

    加入專案檔案。

---
+++ Net MVC Workshop2/Controllers/BookController.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Net_MVC_Workshop2.Controllers
+{
+    public class BookController : Controller
+    {
+        // GET: Book
+        [HttpGet()]
+        public ActionResult Index()
+        {
+
+            return View();
+            //return this.Json(ViewBag.SearchResult ,JsonRequestBehavior.AllowGet);
+            //return Json(result, JsonRequestBehavior.AllowGet);
+        }
+        [HttpPost()]
+        public JsonResult Index(Models.BookSearchArg book)
+        {
+            Models.BookServices bookService = new Models.BookServices();
+            ViewBag.SearchResult = bookService.GetBookByCondtioin(book);
+            var result = bookService.GetBookByCondtioin(book);
+            return Json(result);
+            //return View();
+            //return this.Json(ViewBag.SearchResult ,JsonRequestBehavior.AllowGet);
+            //return Json(result, JsonRequestBehavior.AllowGet);
+        }
+        [HttpPost()]
+        public JsonResult BookClass(Models.BookClass bookclass)
+        {
+            Models.BookServices bookService = new Models.BookServices();
+            var result = bookService.GetBookClass(bookclass);
+            return Json(result);
+        }
+        [HttpPost()]
+        public JsonResult BookKeeper(Models.BookKeeper bookkeeper)
+        {
+            Models.BookServices bookService = new Models.BookServices();
+            var result = bookService.GetBookKeeper(bookkeeper);
+            return Json(result);
+        }
+        [HttpPost()]
+        public JsonResult BookStatus(Models.BookStatus bookstatus)
+        {
+            Models.BookServices bookService = new Models.BookServices();
+            var result = bookService.GetBookStatus(bookstatus);
+            return Json(result);
+        }
+
+
+        [HttpPost()]
+        public JsonResult Insert(Models.BookSearchArg Book)
+        {
+            
+                Models.BookServices bookService = new Models.BookServices();
+
+                bookService.InsertBook(Book);
+                //TempData["message"] = "存檔成功";
+           
+            return Json(Book);
+        }
+
+        [HttpPost()]
+        public JsonResult Delete(string BookId)
+        {
+            try
+            {
+                Models.BookServices bookService = new Models.BookServices();
+                bookService.DeleteBookById(BookId);
+                return this.Json(true);
+            }
+
+            catch (Exception ex)
+            {
+                return this.Json(false);
+            }
+        }
+
+        [HttpGet()]
+        public JsonResult Update(string BookId)
+        {
+            try
+            {
+                Models.BookServices bookService = new Models.BookServices();
+                bookService.GetBookUpdateByCondtioin(BookId);
+                var result = bookService.GetBookUpdateByCondtioin(BookId);
+                return Json(result, JsonRequestBehavior.AllowGet);
+                //return this.Json(true,);
+
+            }
+
+            catch (Exception ex)
+            {
+                return this.Json(false);
+            }
+        }
+
+        [HttpPost()]
+        public JsonResult Update(Models.BookSearchArg Book)
+        {
+            try
+            {
+                Models.BookServices bookService = new Models.BookServices();
+                bookService.UpdateBookById(Book);
+                return this.Json(true);
+            }
+
+            catch (Exception ex)
+            {
+                return this.Json(false);
+            }
+        }
+
+
+    }
+}
